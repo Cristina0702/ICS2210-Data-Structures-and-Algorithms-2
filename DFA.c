54b368fbeffaa51f60d55b9d1c72362f3e067e22
Cristina Cutajar <cristinacutajar@gmail.com>
2022-04-01 12:20:18 +0200

    Code Update

--- DFA.c
+++ DFA.c
@@ -1,35 +1,68 @@
+#include "DFA.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define MAX_STATES 64
+states_ptr *create_states(int n, int i)
+{
+    dfa_ptr *dfa;
 
-typedef struct states{
-    char state;
-    int a; //outgoing transition 
-    int b; //outgoing transition
-    bool accepting; // = false;
-} states_ptr;
+    states_ptr *new_state = (states_ptr*)malloc(sizeof(states_ptr));
+    new_state->content = i;
+    new_state->accepting = flip_coin();
 
-typedef struct dfa{
-    states_ptr *states[MAX_STATES]; //= {0}; //K  //size n
-    char *alphabet; //= {0}; //T
-    //char *transitions;
-    int initial;
-    int *final; //F
-} *dfa_ptr;
+    int temp = rand() % n; //random number between 0 and n
+    while(dfa->states[temp]->accepting == false){
+        //looping until an accepting state is found
+        temp = rand() % n;
+    }
+    new_state->a = temp;
+    
+    //making sure that a and b are different states
+    while ((temp == new_state->a) || (dfa->states[temp]->accepting == false)){
+        //looping until an accepting state is found which is a different state from a
+        temp = rand() % n;
+    }
+    new_state->b = temp;
+    
+	return new_state;
+}
 
-dfa_ptr init(int n){
+bool flip_coin(){
+    int coin = rand() % 2; //either 1 or 0
+    if (coin == 0){
+        return true;
+    }else if(coin == 1){
+        return false;
+    }
+}
+
+dfa_ptr* init(int n){
     if(n >= 16 && n <= 64){
         //if n is between 16 and 64
-        dfa_ptr dfa = malloc(sizeof())
+        dfa_ptr *dfa = (dfa_ptr*)malloc(sizeof(dfa_ptr));
+        dfa->count = 0;
+        dfa->initial = -1;
+        dfa->current = -1;
+
+        for(int i=0; i<n; i ++){
+            dfa->states[i] = create_states(n, i);
+        }
+
+        return dfa;
     }else{
         return NULL;
     }
 }
 
+void reset(dfa_ptr *dfa)
+{
+	dfa->current = dfa->initial;
+}
+
 void main(){
-    dfa_ptr dfa;
+    dfa_ptr *dfa;
     
     int n = rand() % 49 + 16; //random number between 16 and 64
     printf("%d \n", n);
--- DFA.h
+++ DFA.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#define MAX_STATES 64
+
+typedef struct states{
+    int content;
+    int a; //outgoing transition 
+    int b; //outgoing transition
+    bool accepting; // = false;
+} states_ptr;
+
+typedef struct dfa{
+    states_ptr *states[MAX_STATES]; //= {0}; //K  //size n
+    int count;
+    char *alphabet; //= {0}; //T
+    //char *transitions;
+    int initial;
+    int current;
+    int *final; //F
+}dfa_ptr;
